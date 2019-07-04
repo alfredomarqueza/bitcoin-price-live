403d92850b9b706e4449f6b3a9aa7fec0270b4df
Alfredo Marquez <alfredo.marquez@gmail.com>
2019-07-04 15:22:47 -0500

    New grid system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import './App.css';
+import { Container, Row, Col } from 'react-grid-system';
 import { Line, Chart } from 'react-chartjs-2';
 import moment from 'moment';
 import 'moment/locale/es';
@@ -17,7 +18,7 @@ class App extends Component {
 
     // chart.js defaults
     Chart.defaults.global.defaultFontColor = '#000';
-    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontSize = 14;
 
     let endDate = new Date();
     let startDate = new Date();
@@ -156,20 +157,20 @@ class App extends Component {
       datasets: [
         {
 
-          label: "Bitcoin",
+          label: "Bitcoin price",
           fill: true,
           lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
+          backgroundColor: 'rgba(100,71,192,0.4)',
+          borderColor: 'rgba(100,71,192,1)',
           borderCapStyle: 'butt',
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBorderColor: 'rgba(100,71,192,1)',
           pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBackgroundColor: 'rgba(100,71,192,1)',
           pointHoverBorderColor: 'rgba(220,220,220,1)',
           pointHoverBorderWidth: 2,
           pointRadius: 1,
@@ -200,60 +201,79 @@ class App extends Component {
     return (
       <div className="app">
         <Header title="BITCOIN PRICE LIVE" />
-
-        <div className="select-container">
-          <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}> Select your currency: </span>
-          <select value={this.state.currency} onChange={this.onCurrencySelect}>
-            {currencies.map((obj, index) =>
-              <option key={`${index}-${obj.country}`} value={obj.currency}> {obj.currency} </option>
-            )}
-          </select>
-          {
-            /* this.state.currency !== 'USD' && (<div>
-              <a href="#" className="link" onClick={() => this.setCurrency('USD')} style={{color: "black", fontSize: 16, fontFamily: 'Arial Black'}}> [CLICK HERE TO RESET] </a>
-            </div>) */
-          }
-        </div>
-
-        <div className="select-container">
-          <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}> Select your culture: </span>
-          <select value={this.state.culture} onChange={this.onCultureSelect}>
-            <option value="en-US"> United States </option>
-            <option value="es"> Spain </option>
-          </select>
-        </div>
-
-        <div style={{ marginTop: 10 }}>          
-          <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}> Start date: </span>
-          <DatePicker
-            selected={this.state.startDate}
-            onChange={this.startDate_handleChange.bind(this)}
-            dateFormat="MMMM d, yyyy"
-            locale={this.state.culture}
-          />
-          <br />
-          <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}> End date: </span>
-          <DatePicker
-            selected={this.state.endDate}
-            onChange={this.endDate_handleChange.bind(this)}
-            dateFormat="MMMM d, yyyy"
-            locale={this.state.culture}
-          />
-          <br />
-          <br />
-          <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}>
-                Last price:&nbsp;  
-                {this.state.currentPriceError? 
-                  (<span>{this.state.currentPriceError}</span>):
-                  (this.state.currentPrice ? (
-                    <span>
-                    <input type="text" className={this.state.currentPriceLabelStyle} value={this.formatCurrectPrice()} readOnly></input>                    
-                    &nbsp;<span style={{ fontSize: 14, fontFamily: 'Arial' }} className={this.state.updatedLabelStyle}>updated!</span>
-                    </span>
-                    ) : (<span>Loading...</span>))
-                }                
-              </span>          
-              
+        <Container  fluid>
+          <Row justify="center">
+            <Col >
+              <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}> Select your currency: </span>
+            </Col>
+            <Col >
+              <select className="select-container" value={this.state.currency} onChange={this.onCurrencySelect}>
+                {currencies.map((obj, index) =>
+                  <option key={`${index}-${obj.country}`} value={obj.currency}> {obj.currency} </option>
+                )}
+              </select>
+              {
+                /* this.state.currency !== 'USD' && (<div>
+                  <a href="#" className="link" onClick={() => this.setCurrency('USD')} style={{color: "black", fontSize: 16, fontFamily: 'Arial Black'}}> [CLICK HERE TO RESET] </a>
+                </div>) */
+              }
+          </Col>
+        </Row>
+        <Row justify="center">
+          <Col >        
+            <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}> Select your culture: </span>
+          </Col>
+          <Col>
+            <select className="select-container" value={this.state.culture} onChange={this.onCultureSelect}>
+              <option value="en-US"> United States </option>
+              <option value="es"> Spain </option>
+            </select>
+          </Col>
+          </Row>
+          <Row justify="center">
+            <Col>        
+              <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}> Start date: </span>
+            </Col>
+            <Col>        
+              <DatePicker
+                selected={this.state.startDate}
+                onChange={this.startDate_handleChange.bind(this)}
+                dateFormat="MMMM d, yyyy"
+                locale={this.state.culture}
+              />
+            </Col>
+          </Row>
+          <Row justify="center">
+            <Col>        
+              <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}> End date: </span>
+            </Col>
+            <Col>        
+              <DatePicker
+                selected={this.state.endDate}
+                onChange={this.endDate_handleChange.bind(this)}
+                dateFormat="MMMM d, yyyy"
+                locale={this.state.culture}
+              />
+            </Col>
+          </Row>
+          <Row justify="center">
+            <Col>        
+              <span style={{ fontSize: 18, fontFamily: 'Arial Black' }}>
+                    Last price:</span>
+            </Col>
+            <Col>        
+              {this.state.currentPriceError? 
+                (<span>{this.state.currentPriceError}</span>):
+                (this.state.currentPrice ? (
+                  <span>
+                  <input type="text" className={this.state.currentPriceLabelStyle} value={this.formatCurrectPrice()} readOnly></input>                    
+                  &nbsp;<span style={{ fontSize: 14, fontFamily: 'Arial' }} className={this.state.updatedLabelStyle}>updated!</span>
+                  </span>
+                  ) : (<span>Loading...</span>))
+              }                                        
+            </Col>
+          </Row>
+        </Container>
           <br />
           {this.state.historicalDataError? 
             (<span>{this.state.historicalDataError}</span>):
@@ -262,7 +282,7 @@ class App extends Component {
               ) : (<span>Loading...</span>)
             )
           }
-        </div>
+        
       </div>
     )
 
